98e2df770faf1b97d6e0ad45af966ae78207b468
imuniks <imuniks1@gmail.com>
2020-11-23 13:26:15 +0200

    add planet project

--- planet/dashboard.js
+++ planet/dashboard.js
@@ -22,46 +22,46 @@ window.onload = () => {
 
         });
     });
-    if (window.innerWidth < 768) {
-        const menu = document.querySelector('.dashboard-aside-wrap');
-        var initialPoint;
-        var finalPoint;
-        document.addEventListener('touchstart', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            initialPoint=event.changedTouches[0];
-        }, false);
-        document.addEventListener('touchend', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            finalPoint=event.changedTouches[0];
-            var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
-            var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
-            if (xAbs > 20 || yAbs > 20) {
-                if (xAbs > yAbs) {
-                    if (finalPoint.pageX < initialPoint.pageX){
-                        menu.style.left = '-263px';
-                        menu.style.zIndex = '99';
-                        window.body.style.overflow = 'hidden';
-                    }
-                    else{
-                        menu.style.left = '0';
-                        menu.style.zIndex = '2';
-                        window.body.style.overflow = 'visible';
-                    }
+
+if (window.innerWidth < 768) {
+    const menu = document.querySelector('.dashboard-aside-wrap');
+    var initialPoint;
+    var finalPoint;
+    document.addEventListener('touchstart', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        initialPoint=event.changedTouches[0];
+    }, false);
+    document.addEventListener('touchend', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        finalPoint=event.changedTouches[0];
+        var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+        var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+        if (xAbs > 20 || yAbs > 20) {
+            if (xAbs > yAbs) {
+                if (finalPoint.pageX < initialPoint.pageX){
+                    menu.style.left = '-90vw';
+                    menu.style.zIndex = '99';
+                    document.body.style.overflow = 'hidden';
                 }
-                else {
-                    if (finalPoint.pageY < initialPoint.pageY){
-                        /*СВАЙП ВВЕРХ*/}
-                    else{
-                        /*СВАЙП ВНИЗ*/}
+                else{
+                    menu.style.left = '0';
+                    menu.style.zIndex = '2';
+                    document.body.style.overflow = 'visible';
                 }
             }
-        }, false);
-    }
+            else {
+                if (finalPoint.pageY < initialPoint.pageY){
+                    /*СВАЙП ВВЕРХ*/}
+                else{
+                    /*СВАЙП ВНИЗ*/}
+            }
+        }
+    }, false);
+}
 
 };
-
 
 
 
